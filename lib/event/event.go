4596dc54b84b00f48a394b3b1e7b990263599d63
Peter Eliasson <warackta@gmail.com>
2016-12-05 17:25:16 +0100

    Revert "Added a timestamp to events"
    Not actually using timestamps (yet?). Lets not add complexity before
    it is needed.

--- lib/event/event.go
+++ lib/event/event.go
@@ -1,28 +1,19 @@
 package event
 
-import (
-	"encoding/json"
-	"time"
-)
+import "encoding/json"
 
 type Event interface {
 	Name() string
-	Timestamp() time.Time
 	Data() interface{}
 }
 
 type event struct {
-	name      string
-	timestamp time.Time
-	data      interface{}
+	name string
+	data interface{}
 }
 
 func (e *event) Name() string {
 	return e.name
-}
-
-func (e *event) Timestamp() time.Time {
-	return e.timestamp
 }
 
 func (e *event) Data() interface{} {
@@ -31,20 +22,17 @@ func (e *event) Data() interface{} {
 
 func (e *event) MarshalJSON() ([]byte, error) {
 	return json.Marshal(&struct {
-		Name      string
-		Timestamp time.Time
-		Data      interface{}
+		Name string
+		Data interface{}
 	}{
-		Name:      e.Name(),
-		Timestamp: e.Timestamp(),
-		Data:      e.Data(),
+		Name: e.Name(),
+		Data: e.Data(),
 	})
 }
 
 func newEvent(name string, data interface{}) *event {
 	return &event{
-		name:      name,
-		timestamp: time.Now(),
-		data:      data,
+		name: name,
+		data: data,
 	}
 }
