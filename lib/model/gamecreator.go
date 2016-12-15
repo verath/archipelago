ec16412ca35683f9bfb2b191041fe75e30bf3a40
Peter Eliasson <warackta@gmail.com>
2016-12-15 13:56:22 +0100

    Games are now created with some randomness

--- lib/model/gamecreator.go
+++ lib/model/gamecreator.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"fmt"
+	"time"
+	"math/rand"
 )
 
 func createPlayers() (p1, p2, pn *Player, err error) {
@@ -23,25 +25,36 @@ func createPlayers() (p1, p2, pn *Player, err error) {
 	return
 }
 
-func createIslands(p1, p2, pn *Player) ([]*Island, error) {
-	islandData := []struct {
-		Pos      Coordinate
+func createIslands(p1, p2, pn *Player, size Coordinate, gameRand *rand.Rand) ([]*Island, error) {
+	type islandData struct {
 		Size     IslandSize
 		Strength int
 		Player   *Player
-	}{
-		{Coordinate{0, 0}, IslandSizeMedium, 10, p1},
-		{Coordinate{6, 6}, IslandSizeMedium, 10, p2},
-		{Coordinate{4, 4}, IslandSizeMedium, 10, pn},
-		{Coordinate{6, 2}, IslandSizeMedium, 10, pn},
-		{Coordinate{2, 6}, IslandSizeMedium, 10, pn},
-		{Coordinate{0, 3}, IslandSizeMedium, 10, pn},
-		{Coordinate{3, 0}, IslandSizeMedium, 10, pn},
+	}
+	islandMap := make(map[Coordinate]islandData)
+
+	islandSizes := []IslandSize{IslandSizeSmall, IslandSizeMedium, IslandSizeLarge}
+	// Randomize the neutral islands
+	for x := 0; x < size.X; x++ {
+		for y := 0; y < size.Y; y ++ {
+			if gameRand.Intn(100) >= 20 {
+				continue
+			}
+			pos := Coordinate{x, y}
+			size := islandSizes[gameRand.Intn(len(islandSizes))]
+			strength := gameRand.Intn(22) + 4 // 4-25
+			islandMap[pos] = islandData{size, strength, pn}
+		}
 	}
 
+	// Set top left to player 1 island, bottom right to player 2 island
+	islandMap[Coordinate{0,0}] = islandData{IslandSizeLarge, 20, p1}
+	islandMap[Coordinate{size.X-1,size.Y-1}] = islandData{IslandSizeLarge, 20, p2}
+
+	// Transform the map to a slice of islands
 	islands := make([]*Island, 0)
-	for _, data := range islandData {
-		island, err := NewIsland(data.Pos, data.Size, data.Strength, data.Player)
+	for pos, data := range islandMap {
+		island, err := NewIsland(pos, data.Size, data.Strength, data.Player)
 		if err != nil {
 			return nil, fmt.Errorf("Error creating island (data: %v): %v", data, err)
 		}
@@ -50,19 +63,23 @@ func createIslands(p1, p2, pn *Player) ([]*Island, error) {
 	return islands, nil
 }
 
-// TODO: this function should be moved somewhere and made configurable
 func CreateBasicGame() (*Game, error) {
+	seed := time.Now().UnixNano()
+	gameRand := rand.New(rand.NewSource(seed))
+
+	size := Coordinate{7, 7}
+
 	p1, p2, pn, err := createPlayers()
 	if err != nil {
 		return nil, fmt.Errorf("Error creating players: %v", err)
 	}
 
-	islands, err := createIslands(p1, p2, pn)
+	islands, err := createIslands(p1, p2, pn, size, gameRand)
 	if err != nil {
 		return nil, fmt.Errorf("Error creating islands: %v", err)
 	}
 
-	gameBuilder := NewGameBuilder(Coordinate{7, 7}, p1, p2, pn)
+	gameBuilder := NewGameBuilder(size, p1, p2, pn)
 	for _, island := range islands {
 		gameBuilder.AddIsland(island)
 	}
