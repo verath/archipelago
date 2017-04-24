5233c1fbd23ef665d1308437a59eee619090f8eb
Peter Eliasson <warackta@gmail.com>
2017-04-24 19:05:29 +0200

    Adds Babel for ES6->ES5

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -6,6 +6,16 @@ module.exports = {
         filename: 'app.bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {presets: ['es2015']}
+            }
+        ]
+    },
     resolve: {
         alias: {
             pixijs: path.resolve(__dirname, 'node_modules/pixi.js'),
