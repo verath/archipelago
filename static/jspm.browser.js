5ad8a5a80b4267f31f0ae1b66edfb851de3f203c
Peter Eliasson <warackta@gmail.com>
2017-04-24 18:21:32 +0200

    Remove JSPM, JSPM related files

--- static/jspm.browser.js
+++
@@ -1,9 +0,0 @@
-SystemJS.config({
-  baseURL: "/",
-  production: false,
-  paths: {
-    "github:": "jspm_packages/github/",
-    "npm:": "jspm_packages/npm/",
-    "archipelago-frontend/": "lib/"
-  }
-});
