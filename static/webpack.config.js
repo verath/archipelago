c87b4e40fb1c650b6867c8202bea0c2f6d0ddcc7
Peter Eliasson <warackta@gmail.com>
2017-04-24 18:59:52 +0200

    Re-adds pixi.js, eventemitter as dependencies

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -5,5 +5,10 @@ module.exports = {
     output: {
         filename: 'app.bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        alias: {
+            pixijs: path.resolve(__dirname, 'node_modules/pixi.js'),
+        }
     }
 };
