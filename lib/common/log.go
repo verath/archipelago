6a6069bfee6183db084e47fd850c3f3ad11d5932
Peter Eliasson <warackta@gmail.com>
2017-05-14 10:58:29 +0200

    Fixes golint lib/common.

--- lib/common/log.go
+++ lib/common/log.go
@@ -4,15 +4,24 @@ import (
 	"github.com/Sirupsen/logrus"
 )
 
-var ModuleNameKey = "module"
+// Field name constants for the field names used on the
+// logger entry.
+const (
+	FieldNameModule = "module"
+	FieldNameID     = "id"
+)
 
+// ModuleLogEntry creates a new logrus entry that includes the module name as a field.
 func ModuleLogEntry(log *logrus.Logger, moduleName string) *logrus.Entry {
-	return log.WithField(ModuleNameKey, moduleName)
+	return log.WithField(FieldNameModule, moduleName)
 }
 
+// ModuleLogEntryWithID creates a new logrus entry that includes both  the module name
+// and a global id as keys. This is useful when multiple instances of the same module
+// will be running as the id can then be used to keep track of each module instance.
 func ModuleLogEntryWithID(log *logrus.Logger, moduleName string) *logrus.Entry {
 	return log.WithFields(logrus.Fields{
-		ModuleNameKey: moduleName,
-		"id":          NextGlobalID(),
+		FieldNameModule: moduleName,
+		FieldNameID:     NextGlobalID(),
 	})
 }
