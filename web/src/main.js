fa2794b95fab595d70b6b8f6f207e29220d9660a
Peter Eliasson <warackta@gmail.com>
2017-07-29 21:37:35 +0200

    Uses different ws endpoint in prod/dev

--- web/src/main.js
+++ web/src/main.js
@@ -17,6 +17,25 @@ OfflinePluginRuntime.install();
  */
 const WS_VERSION = "2";
 
+/**
+ * The host for the websocket endpoint in production
+ * @type {string}
+ */
+const WS_HOST_PROD = "ws.playarchipelago.com";
+
+/**
+ * The host for the websocket endpoint in development
+ * @type {string}
+ */
+const WS_HOST_DEV = "127.0.0.1:8080";
+
+/**
+ * List of hostnames that indicates that the site is hosted locally and
+ * should connect to the development websocket endpoint.
+ * @type {[string]}
+ */
+const DEVELOPMENT_HOSTNAMES = ["localhost", "127.0.0.1"];
+
 class Main {
 
     /**
@@ -75,8 +94,9 @@ class Main {
     run() {
         this._progressText.setText("Finding a game");
 
-        let protocol = (location.protocol === "https:") ? "wss://" : "ws://";
-        let connection = new Connection(protocol + location.host + "/ws?v=" + WS_VERSION);
+        let protocol = (window.location.protocol === "https:") ? "wss://" : "ws://";
+        let host = (DEVELOPMENT_HOSTNAMES.indexOf(window.location.hostname) === -1) ? WS_HOST_PROD : WS_HOST_DEV;
+        let connection = new Connection(protocol + host + "/ws?v=" + WS_VERSION);
         let gameModel = new GameModel();
         let gameView = new GameView(this._resourceHolder, this._renderer, gameModel);
         this._gameController = new GameController(connection, gameModel, gameView);
