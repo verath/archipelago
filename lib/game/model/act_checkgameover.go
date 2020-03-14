518556ca8aac92c5ef81656817f2d02e083b7e36
Peter Eliasson <warackta@gmail.com>
2020-03-14 07:48:52 +0100

    Track player alive state

--- lib/game/model/act_checkgameover.go
+++ lib/game/model/act_checkgameover.go
@@ -14,9 +14,9 @@ func (act *ActionCheckGameOver) Apply(game *Game) ([]Event, error) {
 	return nil, nil
 }
 
-// isAlive returns true if player p is still alive in game g. An alive player
-// has control of at least one island or airplane.
-func isAlive(g *Game, p *Player) bool {
+// checkAlive returns true if player p is still alive in game g. An alive
+// player has control of at least one island or airplane.
+func checkAlive(g *Game, p *Player) bool {
 	for _, island := range g.Islands() {
 		if island.Owner().Equals(p) {
 			return true
@@ -36,12 +36,16 @@ func isAlive(g *Game, p *Player) bool {
 // winner.
 // If no players are alive then nil is returned as winner.
 func isGameOver(g *Game) (gameOver bool, winner *Player) {
-	// TODO: Add isDead flag on Player to skip checking known dead.
 	alivePlayers := make([]*Player, 0)
 	for _, player := range g.Players() {
-		if isAlive(g, player) {
-			alivePlayers = append(alivePlayers, player)
+		if !player.IsAlive() {
+			continue
 		}
+		if !checkAlive(g, player) {
+			player.SetAlive(false)
+			continue
+		}
+		alivePlayers = append(alivePlayers, player)
 	}
 	if len(alivePlayers) == 0 {
 		return true, nil
--- lib/game/model/player.go
+++ lib/game/model/player.go
@@ -3,19 +3,34 @@ package model
 // Player represents a player in the game model.
 type Player struct {
 	id PlayerID
+	// alive is set false when the player is no longer alive in the game. Used
+	// to short-circuit logic that only applies to alive players.
+	alive bool
 }
 
 // NewPlayer creates a new Player with a new generated ID.
 func NewPlayer() (*Player, error) {
 	id := PlayerID(NextModelID())
 	return &Player{
-		id: id,
+		id:    id,
+		alive: true,
 	}, nil
 }
 
 // ID returns the PlayerID of the Player.
 func (p *Player) ID() PlayerID {
 	return p.id
+}
+
+// IsAlive returns the alive state of the player, i.e. if the player is still
+// alive in the game.
+func (p *Player) IsAlive() bool {
+	return p.alive
+}
+
+// SetAlive sets the alive state of the player.
+func (p *Player) SetAlive(alive bool) {
+	p.alive = alive
 }
 
 // Equals compares two players for equality. Equality is determined
@@ -27,6 +42,7 @@ func (p *Player) Equals(other *Player) bool {
 // Copy performs a deep copy of the Player, returning the copy.
 func (p *Player) Copy() *Player {
 	return &Player{
-		id: p.id,
+		id:    p.id,
+		alive: p.alive,
 	}
 }
