ac0ddb045541eae2c728abb3a080efdceebe95b4
Peter Eliasson <warackta@gmail.com>
2020-07-03 14:25:15 +0200

    Server: Use base 36 for ids

--- lib/common/id.go
+++ lib/common/id.go
@@ -9,8 +9,8 @@ var idCount uint64
 
 // NextGlobalID returns the next value of a global id counter,
 // shared between any caller to this method. The returned id is
-// returned as a base16 string.
+// returned as a base36 string.
 func NextGlobalID() string {
 	val := atomic.AddUint64(&idCount, uint64(1))
-	return strconv.FormatUint(val, 16)
+	return strconv.FormatUint(val, 36)
 }
